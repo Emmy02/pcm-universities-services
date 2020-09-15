4fe0b04b6c9fb5e8265d28090904c1bdfeff7d21
Enmanuel Alejandro De Oleo <emmyu.d@gmail.com>
2020-09-15 18:41:05 -0500

    fixing chat bug

--- conn.js
+++ conn.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const db_path = "mongodb://localhost/express-mongo";
+const db_path = "mongodb://localhost:27017/express-mongo";
 
 const config = {
   useUnifiedTopology: true,
