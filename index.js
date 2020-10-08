c06b219f1d881760827e6ced77b3b5a3a366decc
Enmanuel Alejandro De Oleo <emmyu.d@gmail.com>
2020-10-08 08:16:32 -0500

    adding cors

--- index.js
+++ index.js
@@ -17,7 +17,6 @@ app.use(
 );
 
 app.use(bodyParser.json());
-app.use(cors());
 
 app.options("*", cors());
 // Importing models and controllers
@@ -51,6 +50,28 @@ universities
   .delete(UniversityCtrl.deleteUniversity);
 
 app.use("/api", universities);
+app.use(cors());
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+
+  // authorized headers for preflight requests
+  // https://developer.mozilla.org/en-US/docs/Glossary/preflight_request
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+
+  app.options("*", (req, res) => {
+    // allowed XHR methods
+    res.header(
+      "Access-Control-Allow-Methods",
+      "GET, PATCH, PUT, POST, DELETE, OPTIONS"
+    );
+    res.send();
+  });
+});
 
 app.listen(port, () => {
   console.log(`Express running on: http://localhost:${port}`);
